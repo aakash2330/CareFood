69586795d4f5c8bda207a23b9650e97efe0d55d7
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-04-03 08:21:25 +0530

    Updated restauarants api

--- src/app/restaurants/components/Body.tsx
+++ src/app/restaurants/components/Body.tsx
@@ -30,10 +30,10 @@ function Body() {
       async function getRestaurants() {
      
         const data = await fetch(
-          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+          " https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
         const json = await data.json();
-      
+       
       
         setRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
    
