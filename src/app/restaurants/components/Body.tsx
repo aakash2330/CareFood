3b9c3ca38271536e913977f764ba01732bc8e38d
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-04-03 08:34:03 +0530

    Updated api

--- src/app/restaurants/components/Body.tsx
+++ src/app/restaurants/components/Body.tsx
@@ -30,7 +30,8 @@ function Body() {
       async function getRestaurants() {
      
         const data = await fetch(
-          " https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+          " https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+          
         );
         const json = await data.json();
        
