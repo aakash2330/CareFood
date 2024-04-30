fed837228d0fe1062a19be06e33d6d7d5b13d924
aakash2330 <aakash2330@gmail.com>
2024-04-30 13:31:04 +0530

    donate feature

--- src/lib/cartSlice.ts
+++ src/lib/cartSlice.ts
@@ -15,9 +15,14 @@ const cartSlice = createSlice({
         removeFromCart(state: { items: any[] }, action) {
             const itemId = action.payload;
             state.items = state.items.filter(item => item?.id !== itemId)
-        }
+        },
+    changeDonationQuantity(state: { items: any[] }, action){
+      console.log(state.items,action.payload)
+      console.log({updatedState : state.items.map((item)=>{return item.card.info.id==action.payload.id ? {...item,donationQuantity:action.payload.donationQuantity}:item})})
+      state.items=state.items.map((item)=>{return item.card.info.id==action.payload.id ? {...item,card:{...item.card , info:{...item.card.info,donationQuantity:action.payload.donationQuantity}}}:item})
+    }
     }
 })
 
-export const { addToCart, clearCart,removeFromCart } = cartSlice.actions
+export const { addToCart, clearCart,removeFromCart,changeDonationQuantity } = cartSlice.actions
 export default cartSlice.reducer
